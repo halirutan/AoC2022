09de80723028f5f113e86351a5461c2634173168
Patrick Scheibe <patrick@halirutan.de>
2022-12-25 13:48:32 +0100

    Part 2 of day 16

--- src/Day16.kt
+++ src/Day16.kt
@@ -1,34 +1,20 @@
 data class Valve(val neighbors: List<String>, val flow: Int, val pressureRelease: Int = 0)
+data class Player(val currentValve: String, val remainingTime: Int)
+
+// We use these for the stack where we store the search progression
+data class OnePlayerGame(val g: GameState, val p1: Player)
+data class TwoPlayerGame(val g: GameState, val p1: Player, val p2: Player)
 
 val globalPathStore = mutableMapOf<Set<String>, Int>()
 
-data class TurnOnAction(val target: String) {
-    override fun toString(): String {
-        return "${this::class.simpleName}($target)"
-    }
-}
+data class ValveOnAction(val target: String)
 
-data class GameState(val currentValve: String, val remainingTime: Int, val valves: Map<String, Valve>) {
+data class GameState(val valves: Map<String, Valve>) {
     val score = valves.values.sumOf { it.pressureRelease }
-
-//    override fun toString(): String {
-//        val builder = StringBuilder()
-//        for (valve in valves) {
-//            builder
-//                .append("Valve ")
-//                .append(valve.key)
-//                .append(" has flow rate=")
-//                .append(valve.value.flow)
-//                .append("; tunnels lead to valves ")
-//                .append(valve.value.neighbors.toString())
-//                .appendLine()
-//        }
-//        return builder.toString()
-//    }
 
     override fun toString(): String {
         val builder = StringBuilder()
-        builder.append("At \"$currentValve\" ")
+        builder.append("Used valves ${valves.values.filter { it.pressureRelease > 0 }.size}:")
         for (valve in valves) {
             if (valve.value.pressureRelease > 0) {
                 builder
@@ -39,27 +25,27 @@ data class GameState(val currentValve: String, val remainingTime: Int, val valve
         return builder.toString()
     }
 
-    fun getPossibleActions(): List<TurnOnAction> {
-        val result = mutableListOf<TurnOnAction>()
+    fun getPossibleActions(): List<ValveOnAction> {
+        val result = mutableListOf<ValveOnAction>()
         for (v in valves.filter { it.value.flow > 0 && it.value.pressureRelease == 0 }) {
-            result.add(TurnOnAction(v.key))
+            result.add(ValveOnAction(v.key))
         }
         return result
     }
 
-    fun doAction(act: TurnOnAction): GameState {
-        assert(remainingTime > 0)
+    fun doAction(act: ValveOnAction, player: Player): Pair<GameState, Player> {
+        assert(player.remainingTime > 0)
         val vToTurnOn = valves[act.target] ?: throw Error("Valve ${act.target} does not exist")
         assert(vToTurnOn.flow > 0)
-        val timeToTravel = findShortestPath(currentValve, act.target)
+        val timeToTravel = findShortestPath(player.currentValve, act.target)
         val newValves = valves.toMutableMap()
-        val remainingTimeAfterAction = remainingTime - timeToTravel - 1
+        val remainingTimeAfterAction = player.remainingTime - timeToTravel - 1
         newValves[act.target] = Valve(
             vToTurnOn.neighbors,
             vToTurnOn.flow,
             remainingTimeAfterAction * vToTurnOn.flow,
         )
-        return GameState(act.target, remainingTimeAfterAction, newValves)
+        return Pair(GameState(newValves), Player(act.target, remainingTimeAfterAction))
     }
 
     /**
@@ -91,9 +77,13 @@ data class GameState(val currentValve: String, val remainingTime: Int, val valve
         }
         throw Error("Could not find path from $valve1 to $valve2")
     }
+
+    fun getUnusedValveValue(): Int {
+        return valves.values.filter { it.pressureRelease == 0 && it.flow > 0 }.sumOf { it.flow }
+    }
 }
 
-fun parseGameState(input: List<String>, initialValve: String, remainingTime: Int): GameState {
+fun parseGameState(input: List<String>): GameState {
     val valves = mutableMapOf<String, Valve>()
     for (line in input) {
         val parts = line.split(", ", " ")
@@ -102,16 +92,15 @@ fun parseGameState(input: List<String>, initialValve: String, remainingTime: Int
         val neighbors = parts.subList(9, parts.size)
         valves[name] = Valve(neighbors, flow)
     }
-    return GameState(initialValve, remainingTime, valves)
+    return GameState(valves)
 }
 
 /**
  * A quick test if the shortest path works
  */
+@Suppress("unused")
 fun checkShortestPath(input: List<String>) {
-    val remainingTime = 30
-    val initialValve = "AA"
-    val game = parseGameState(input, initialValve, remainingTime)
+    val game = parseGameState(input)
     println(game.findShortestPath("AA", "II"))
     println(game.findShortestPath("AA", "HH"))
     println(game.findShortestPath("AA", "JJ"))
@@ -119,35 +108,110 @@ fun checkShortestPath(input: List<String>) {
 }
 
 fun main() {
-    print("What???")
     val smallInput = readInput("Day16_test")
     val realInput = readInput("Day16")
 
     fun part1(input: List<String>) {
         val remainingTime = 30
         val initialValve = "AA"
-        val game = parseGameState(input, initialValve, remainingTime)
-        val stack = ArrayDeque<GameState>(1000)
-        stack.add(game)
+        val me = Player(initialValve, remainingTime)
+        val game = parseGameState(input)
+        val stack = ArrayDeque<OnePlayerGame>()
+        stack.add(OnePlayerGame(game, me))
         var maxScore = 0
         while (stack.isNotEmpty()) {
-            val g = stack.removeFirst()
-            if (g.remainingTime < 0) {
+            val (g, p) = stack.removeFirst()
+            if (p.remainingTime < 0) {
                 continue
             }
             val score = g.score
             if (score > maxScore) {
                 maxScore = score
-                println(g)
-                println("New High Score: $score (stack size ${stack.size})")
-
+                print(".")
             }
 
-            stack.addAll(g.getPossibleActions().map { g.doAction(it) })
+            g.getPossibleActions().map { g.doAction(it, p) }.forEach { stack.addFirst(OnePlayerGame(it.first, it.second)) }
         }
+        println()
+        println("Best Score Part 1: $maxScore")
+
     }
 
-//    checkShortestPath(smallInput)
-    part1(realInput)
+    /**
+     * For part 2 we do the same as for part 1, but we just alternate two players between each move.
+     */
+    fun part2(input: List<String>) {
+        val remainingTime = 26
+        val initialValve = "AA"
 
+
+            val me = Player(initialValve, remainingTime)
+            val elephant = Player(initialValve, remainingTime)
+            val game = parseGameState(input)
+
+            val stack = ArrayDeque<TwoPlayerGame>()
+            stack.add(TwoPlayerGame(game, me, elephant))
+            var maxScore = 0
+
+            var iter = 0
+            while (stack.isNotEmpty()) {
+                val (g, p1, p2) = stack.removeFirst()
+                val score = g.score
+
+                if (p1.remainingTime < 0 || p2.remainingTime < 0) {
+                    continue
+                }
+
+                // We over-estimate what additional score we can get from the remaining closed valves.
+                // If it can't get bigger than our current max score, we can prune this whole subtree.
+                val remainingValueEstimate = g.getUnusedValveValue() * (p1.remainingTime+p2.remainingTime)/2
+                if (score + remainingValueEstimate < maxScore) {
+                    continue
+                }
+
+
+                if (score > maxScore) {
+                    maxScore = score
+                    print(".")
+                }
+
+                // Alternating moves between the player and the elephant
+                if (iter.mod(2) == 0) {
+                    g
+                        .getPossibleActions()
+                        .map { g.doAction(it, p1) }
+                        .forEach { stack.addFirst(TwoPlayerGame(it.first, it.second, p2)) }
+
+                }else{
+                    g
+                        .getPossibleActions()
+                        .map { g.doAction(it, p2) }
+                        .forEach { stack.addFirst(TwoPlayerGame(it.first, p1, it.second)) }
+                }
+                iter++
+            }
+        println()
+        println("Best Score Part 2: $maxScore")
+
+    }
+
+    println("Part 1 Small Input")
+    part1(smallInput)
+    println()
+    println()
+
+    println("Part 1 Real Input")
+    part1(realInput)
+    println()
+    println()
+
+    println("Part 2 Small Input")
+    part2(smallInput)
+    println()
+    println()
+
+    println("Part 2 Real Input")
+    part2(realInput)
+    println()
+    println()
 }
